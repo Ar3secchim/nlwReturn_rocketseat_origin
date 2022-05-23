fa5fb2e65c5646b1c88b7ba9f19f255cf4befea1
unknown <renarasecchim@gmail.com>
2022-05-23 17:33:59 -0300

    uptade-color

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
+// add event listeners page all
+window.addEventListener('scroll', onScroll);
+
+onScroll()
 function onScroll(){
-  
+  showNavOnScroll()
+  showBackTopButtonOnScroll()
+}
+
+function showNavOnScroll(){
   if(scrollY > 0){
     navigation.classList.add('scroll')
   } else {
     navigation.classList.remove('scroll')
+  }
+}
+
+function showBackTopButtonOnScroll(){
+  if(scrollY > 500){
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
   }
 }
 
@@ -19,13 +35,13 @@ function closeMenu(){
 ScrollReveal({
   origin:'top',
   distance: '50px',
-  duration:700,
+  duration:800,
 }).reveal(`#home,
-#home img, 
-#home .stats,
-#services header,
-#services .services-section,
-#about header,
-#about .content,
-#contact header,
+  #home img, 
+  #home .stats,
+  #services header,
+  #services .services-section,
+  #about header,
+  #about .content,
+  #contact header
 `)
